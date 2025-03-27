2494a9509a81e142903efbba5a66faab704a9de9
Laba Kumar Deka <labakumardeka42@gmail.com>
2025-03-27 00:06:14 -0400

    added new links

--- background.js
+++ background.js
@@ -1,17 +1,16 @@
 // The URL you want to open every 20 minutes
 const HANSHAKE_ON_CAMPUS = "https://nyu.joinhandshake.com/stu/postings?page=1&per_page=25&sort_direction=desc&sort_column=created_at&job.job_types%5B%5D=6&employers%5B%5D=416439";
-const LINKEDIN_TOP_RECOMMENDED = "https://www.linkedin.com/jobs/collections/recommended/?currentJobId=4143567273&discover=recommended&discoveryOrigin=JOBS_HOME_JYMBII"
 const DATA_INTERN = "https://www.linkedin.com/jobs/search/?alertAction=viewjobs&currentJobId=4074992886&f_TPR=a1739256844-&origin=JOB_ALERT_EMAIL&savedSearchId=14531571252&trk=eml-email_job_alert_digest_01-job~alert-0-see~all~jobs"
-const HANDSHAKE_INTERNSHIPS = "https://nyu.joinhandshake.com/stu/postings?page=1&per_page=25&sort_direction=desc&sort_column=created_at&job.job_types%5B%5D=3&job.salary_types%5B%5D=1&job.job_applicant_preference.accepts_opt_candidates=true&job.job_applicant_preference.accepts_cpt_candidates=true&job.job_applicant_preference.willing_to_sponsor_candidate=true&preset_filters%5B%5D=individual_major_group"
-const GOOGLE_INTERNSHIPS = "https://www.google.com/search?q=software+internships+new+york+in+the+last+3+days&rciv=jb&clksrc=alertsemail&hl=en&gl=US&udm=8&uds=ABqPDvzK7_emtKEDgGqKgRlezqlxz-pnuFfLi5FpxFon478p3hX3oTmgX_rwhfdPsOioI-P3CCsQREYqyzKKGJ9dLmLdJJJ0kx3ugylmomjeQs7eAqCrEnqG40r-AcPp8nphYZYJD2BnvIuwz20g7F0E_witPPPKRekPGJstLcIp4XIKsErUvmGJ3cryYELGkkgWeVRWZjk3w-SugQO09BEseMC7kj5NFRCVRJCyKNlhyJXHWOODU5yRNMpz6tArDvKFtsjSCWnzBcPKVW7ququMhk5bxVP80HJ3MZXMyL2GYLr3Lp1MPZUzXZWZOHvzcL57k9XtOi-v&jbr=sep:0"
-const SIMPLIFY_GITHUB = "https://github.com/SimplifyJobs/Summer2025-Internships"
-const LINKEDIN = "https://www.linkedin.com/jobs/search/?alertAction=viewjobs&currentJobId=4127868351&f_TPR=a1739256844-&geoId=103644278&keywords=intern%20&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true&trk=eml-email_job_alert_digest_01-job~alert-0-see~all~jobs"
-const LATEST = "https://www.linkedin.com/jobs/search/?alertAction=viewjobs&currentJobId=4150534422&f_EA=true&f_TPR=a1739256844-&geoId=103644278&keywords=intern%20&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true&sortBy=R&trk=eml-email_job_alert_digest_01-job~alert-0-see~all~jobs"
+const LATEST = "https://www.linkedin.com/jobs/search/?currentJobId=4190127948&distance=25&f_TPR=r3600&geoId=103644278&keywords=software%20%20intern&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true"
+const LATEST2 = "https://www.linkedin.com/jobs/search/?currentJobId=4190127948&distance=25&f_TPR=r7200&geoId=103644278&keywords=software%20%20intern&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true"
+const LATEST3= "https://www.linkedin.com/jobs/search/?currentJobId=4190127948&distance=25&f_TPR=r14400&geoId=103644278&keywords=software%20%20intern&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true"
 const AMAZON = "https://www.amazon.jobs/en/search?offset=0&result_limit=10&sort=recent&distanceType=Mi&radius=24km&latitude=38.89037&longitude=-77.03196&loc_group_id=&loc_query=united%20states&base_query=software%20intern&city=&country=USA&region=&county=&query_options=&"
-
-
+const JOBRIGHT = "https://jobright.ai/jobs/recommend"
+const INTERN_LIST = "https://www.intern-list.com/"
+const LATEST4= "https://www.linkedin.com/jobs/search/?currentJobId=4190127948&distance=25&f_TPR=86400&geoId=103644278&keywords=software%20%20intern&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true"
+const SIMPLIFY = "https://github.com/SimplifyJobs/Summer2025-Internships"
 const ALARM_NAME = "openUrlAlarm";
-const INTERVAL_MINUTES = 45; // period in minutes
+const INTERVAL_MINUTES = ; // period in minutes
 const DELAY_MS = 2000; // 2 seconds
 
 console.log("Background script running...");
@@ -43,15 +42,16 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     console.log("Alarm triggered! Opening URLs sequentially...");
     
     const urls = [
-      AMAZON,
       LATEST,
-      LINKEDIN,
+      LATEST2,
+      LATEST3,
+      AMAZON,
       DATA_INTERN,
-      HANDSHAKE_INTERNSHIPS,
-      GOOGLE_INTERNSHIPS,
-      SIMPLIFY_GITHUB,
-      LINKEDIN_TOP_RECOMMENDED,
-      HANSHAKE_ON_CAMPUS
+      JOBRIGHT,
+      INTERN_LIST,
+      LATEST4,
+      SIMPLIFY,
+      HANSHAKE_ON_CAMPUS,
     ];
     
     openUrlsSequentially(urls);
